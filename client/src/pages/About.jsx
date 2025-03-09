382dd7582dacb3092328555ef8de011813c7c737
AntaraArifa <antara5643@gmail.com>
2025-03-09 23:00:23 +0600

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      123456789
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>
+            Welcome to Antara's Blog
+          </h1>
+          <div className='text-md text-gray-650 flex flex-col gap-6'>
+            <p>
+              Hi! I’m Antara Arifa Mullick, a third-year software engineering student at the Islamic University of Technology. This blog is my creative space, where I share my thoughts, experiences, and knowledge on a variety of topics that I'm passionate about. From technology and education to personal development and lifestyle, there’s something for everyone here.
+            </p>
+
+            <p>
+              The aim of Antara's Blog is to create a space where readers can learn, explore, and grow. I believe in the power of sharing knowledge, whether it's diving into the latest technological innovations, discovering tips for mental well-being, exploring new places, or simply reflecting on life’s small yet meaningful moments. 
+            </p>
+
+            <p>
+              Each post is crafted with the intention to inspire and educate. I write in a way that encourages exploration, helping you discover new ideas, broaden your horizons, and challenge yourself to think in creative ways. No matter which topic piques your interest, my hope is that you'll walk away from each post having learned something valuable.
+            </p>
+
+            <p>
+              I encourage you to leave comments, share your thoughts, and engage with fellow readers. This is a community built on the exchange of ideas and learning together. So, whether you’re here to expand your knowledge or simply to enjoy a good read, I hope my blog brings something positive to your day!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
